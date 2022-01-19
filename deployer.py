755e53ff028c43e0e166152b205c46efa543ca80
jjohnston@cpacketnetworks.com <d6FVnM5FG>
2022-01-19 17:40:16 -0600

    fix environment passing for password field

--- deployer.py
+++ deployer.py
@@ -247,7 +247,7 @@ if debug: print(cvu_tool_ip)
 num_tools = len(cvu_tool_ip)
 
 user = environ.get('CPKT_USER') if environ.get('CPKT_USER') is not None else get_user("cClear Web UI Username: ")
-password = environ.get('CPKT_PASSWORD') if environ.get('CPKT_USER') is not None else get_passwd("cClear Web UI Password: ")
+password = environ.get('CPKT_PASSWORD') if environ.get('CPKT_PASSWORD') is not None else get_passwd("cClear Web UI Password: ")
 
 # main 
 cur_ss_cvu = get_system_settings(cvu_provisioning)
