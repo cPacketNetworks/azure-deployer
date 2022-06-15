63dcdc60b23e9cc1b3cda4ce6ada3c09710937d2
Jayme Johnston <74691149+jjohnston-cpacket@users.noreply.github.com>
2022-06-14 23:32:34 -0500

    Fix race condition on container creation
    - fix race condition when container creation isn't finished before starting transfer
    - clean up copy and image creation
    - verify container removal

--- createVHDs.bash
+++ createVHDs.bash
@@ -5,32 +5,53 @@ set -e
 
 function get_filename {
     # remove the SAS
-    url=${1%\?*}
+    local url=${1%\?*}
     # remove to last /
     echo "${url##*/}"
 }
 
 function get_account_name {
     # remove the SAS
-    url=${1%\?*}
-    front=${url%%\.*}
+    local url=${1%\?*}
+    local front=${url%%\.*}
     echo "${front##*/}"
 }
 
 function get_container_name {
     # remove the SAS
-    url=${1%\?*}
-    tmp=${url%/*}
+    local url=${1%\?*}
+    local tmp=${url%/*}
     echo ${tmp##*/}
 }
 
 function get_tld {
-    url=${1#*.}
+    local url=${1#*.}
     echo "${url%%/*}"
 }
 
+function container_is_created {
+    local container_created=$(az storage container exists --output tsv --only-show-errors --account-name $my_account_name --name $my_container_name)
+    if [ $container_created == "True" ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function copy_create_image {
+    if [ ! -z "$1" ]; then
+        local imagename=$(get_filename "$1")
+        local vhd_dest_url="https://$my_account_name.$storage_base_name/$my_container_name/$imagename"
+        echo "copying $imagename"
+        azcopy copy "$1" "$vhd_dest_url?$my_sas_token"
+        echo "creating image $imagename"
+        az image create --resource-group "$my_image_rg" --location "$my_image_loc" --name "$imagename" --os-type Linux --source "$vhd_dest_url"
+    fi
+}
+
 cpacket_uri_file=${1:-'./cpacket_uri.txt'}
-my_container_name="tmpcpacketvhds"
+my_container_name="cpacketvhdstemp"
 uri_prompt=1
 
 if [ -r $cpacket_uri_file ]; then
@@ -46,25 +67,20 @@ echo "This storage account MUST be publicly accessable. In the portal under the 
 echo "" 
 if [ $uri_prompt -eq 1 ]; then
     read -ep "Enter your existing storage account name: " -i "$my_account_name" my_account_name
-    read -ep "Enter the storage account subscription Id: " -i "$my_subscription_id" my_subscription_id
-    read -ep "Enter the storage account resource group name: " -i "$my_resource_group" my_resource_group
 fi
-storage_account_ids="/subscriptions/$my_subscription_id/resourceGroups/$my_resource_group/providers/Microsoft.Storage/storageAccounts/$my_account_name"
-echo "The storage resource id is: $storage_account_ids"
-
+my_resource_group=$(az storage account show --output json --only-show-errors --name $my_account_name | jq .resourceGroup | tr -d '"')
 my_image_rg="$my_resource_group"
-my_image_loc=$(az storage account show --output json --only-show-errors --ids $storage_account_ids | jq .location | tr -d '"')
-my_blob_url=$(az storage account show --output json --only-show-errors --ids $storage_account_ids | jq .primaryEndpoints.blob | tr -d '"')
+my_image_loc=$(az storage account show --output json --only-show-errors --name $my_account_name | jq .location | tr -d '"')
+my_blob_url=$(az storage account show --output json --only-show-errors --name $my_account_name | jq .primaryEndpoints.blob | tr -d '"')
 storage_base_name=$(get_tld "$my_blob_url")
 
-
-echo "Checking for existing temporary container"
-container_is_created=$(az storage container exists --output tsv --only-show-errors --subscription $my_subscription_id --account-name $my_account_name --name $my_container_name)
-if [ $container_is_created == "True" ]; then
-    previous_container_deleted=$(az storage container delete --output tsv --only-show-errors --subscription $my_subscription_id --account-name $my_account_name --name $my_container_name)
-    echo "Removing previous temp directory $my_container_name.  Please retry in a couple of mins."
-    exit
-fi
+echo -n "creating temporary storage container $my_container_name..."
+until container_is_created; do
+    echo -n "."
+    container_create=$(az storage container create --output tsv --only-show-errors --account-name $my_account_name --name $my_container_name --resource-group $my_resource_group)
+    sleep 1
+done
+echo "Done"
 
 if [ $uri_prompt -eq 1 ]; then
      echo ""
@@ -74,43 +90,24 @@ if [ $uri_prompt -eq 1 ]; then
      read -ep "Enter the CVU-V URI you recieved from cPacket Networks. Leave blank if you didn't get a CVU-V URI: " -i "$cvu_uri" cvu_uri
 fi
 
-echo "creating temporary storage container"
-container_create=$(az storage container create --output tsv --only-show-errors --subscription $my_subscription_id --account-name $my_account_name --name $my_container_name --resource-group $my_resource_group)
-
 echo "creating 24 hour expiry date"
 my_sas_expiry=$(date --date="+24 hours" +"%Y-%m-%dT%H:%M:%SZ")
 
 echo "generating sas" 
-my_sas_token=$(az storage container generate-sas --only-show-errors --subscription $my_subscription_id --account-name "$my_account_name" --name "$my_container_name" --permissions acw --expiry "$my_sas_expiry" | tr -d '"')
+my_sas_token=$(az storage container generate-sas --only-show-errors --account-name "$my_account_name" --name "$my_container_name" --permissions acw --expiry "$my_sas_expiry" | tr -d '"')
 
-my_container_url="https://$my_account_name.$storage_base_name/$my_container_name?$my_sas_token"
+copy_create_image "$cclear_uri"
+copy_create_image "$cstor_uri"
+copy_create_image "$cvu_uri"
 
-if [ ! -z "$cclear_uri" ]; then
-    echo "copying cclear vhd"
-    azcopy copy "$cclear_uri" "$my_container_url"
-    cclearimagename=$(get_filename "$cclear_uri")
-    echo "creating cclear image"
-    az image create --subscription "$my_subscription_id" --resource-group "$my_image_rg" --location "$my_image_loc" --name "$cclearimagename" --os-type Linux --source "https://$my_account_name.$storage_base_name/$my_container_name/$cclearimagename"
-fi
+echo -n "Removing temporary storage container $my_container_name..."
+until container_is_created; [ "$?" -eq 1 ]; do
+    echo -n "."
+    container_deleted=$(az storage container delete --output tsv --only-show-errors --account-name $my_account_name --name $my_container_name)
+    sleep 1
+done
+echo "Done"
 
-if [ ! -z "$cstor_uri" ]; then
-    echo "copying cstor vhd"
-    azcopy copy "$cstor_uri" "$my_container_url"
-    cstorimagename=$(get_filename "$cstor_uri")
-    echo "creating cstor image"
-    az image create --subscription "$my_subscription_id" --resource-group "$my_image_rg" --location "$my_image_loc" --name "$cstorimagename" --os-type Linux --source "https://$my_account_name.$storage_base_name/$my_container_name/$cstorimagename"
-fi
-
-if [ ! -z "$cvu_uri" ]; then
-    echo "copying cvu vhd"
-    azcopy copy "$cvu_uri" "$my_container_url"
-    cvuimagename=$(get_filename "$cvu_uri")
-    echo "creating cvu image"
-    az image create --subscription "$my_subscription_id" --resource-group "$my_image_rg" --location "$my_image_loc" --name "$cvuimagename" --os-type Linux --source "https://$my_account_name.$storage_base_name/$my_container_name/$cvuimagename"
-fi
-
-container_deleted=$(az storage container delete --output tsv --only-show-errors --subscription $my_subscription_id --account-name $my_account_name --name $my_container_name)
 echo ""
-echo "temporary container removed: $container_deleted"
 
-az image list --output table --only-show-errors --subscription "$my_subscription_id" --resource-group "$my_image_rg"
+az image list --output table --only-show-errors --resource-group $my_image_rg
