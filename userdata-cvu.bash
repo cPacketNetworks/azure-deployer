2a83b4562d2fb980c132bdfb1a9c84535cb0d15e
Josh Sternfeld <Joshuasternfeld98@gmail.com>
2023-05-16 15:24:45 -0400

    Updated the azure deployer to work with 22.3.1., and added various features.
    * Updated the user data to work with 22.3.1.
    
    * Update userdata-cvu.bash
    
    * Update README.md
    
    * Update createUiDefinition.json
    
    * Update README.md
    
    * Update createUiDefinition.json
    
    * Update README.md
    
    * Update createUiDefinition.json
    
    * Updated cVu Logic
    
    * Update userdata-cvu.bash
    
    * Update main.json
    
    * Update userdata-cvu.bash
    
    * Updated the user data
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update userdata-cvu.bash
    
    * updated quotes
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * Update main.json
    
    * unsure if this works
    
    * reverting back
    
    * reverting back to 3b6e41e
    
    * trying with fix for ""s around cvuv_vxlan_remoteip_$1
    
    Note that I replaced the single quotes around the cvuv_vxlan_remoteip_$i field with double quotes and used the backslash to escape the double quotes around the sed pattern. This should allow the shell to substitute the IP address values correctly.
    
    * fixed the cstorlbname issue
    
    * getting experimntal, the last build worked for source ip, not remote
    
    * Update main.json
    
    * attempting to revert to fix one of 2 issues. source / remote
    
    * reverting back to the start of ip addr
    
    "$cvuv_user_data": "capture_nic_ip=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)\ncat >/home/cpacket/boot_config.toml <<EOF_BOOTCFG\n{ \"vm_type\" : \"azure\", \"capture_mode\" : \"cvuv\", \"cvuv_mode\" : \"inline\", \"cvuv_inline_mode\" : \"tctap\", \"cvuv_mirror_eth_0\" : \"eth0\", \"cvuv_vxlan_srcip_0\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_0\" : \"\", \"cvuv_vxlan_id_0\" : \"1337\", \"cvuv_vxlan_srcip_1\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_1\" : \"\", \"cvuv_vxlan_id_1\" : \"1338\", \"cvuv_vxlan_srcip_2\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_2\" : \"\", \"cvuv_vxlan_id_2\" : \"1339\", \"cvuv_vxlan_srcip_3\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_3\" : \"\", \"cvuv_vxlan_id_3\" : \"1340\", \"cvuv_vxlan_srcip_4\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_4\" : \"\", \"cvuv_vxlan_id_4\" : \"1341\", \"cvuv_vxlan_srcip_5\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_5\" : \"\", \"cvuv_vxlan_id_5\" : \"1342\", \"cvuv_vxlan_srcip_6\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_6\" : \"\", \"cvuv_vxlan_id_6\" : \"1343\", \"cvuv_vxlan_srcip_7\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_7\" : \"\", \"cvuv_vxlan_id_7\" : \"1344\", \"cvuv_vxlan_srcip_8\" : \"$capture_nic_ip\", \"cvuv_vxlan_remoteip_8\" : \"\", \"cvuv_vxlan_id_8\" : \"1345\" }\nEOF_BOOTCFG\nIFS=' ' read -ra ADDR <<< \"$REMOTE_IPS\"\nfor i in \"${!ADDR[@]}\"; do\n  sed -i \"s|'cvuv_vxlan_remoteip_$i' : '',|'cvuv_vxlan_remoteip_$i' : '${ADDR[i]}',|\" /home/cpacket/boot_config.toml\ndone\nchmod ug+w /home/cpacket/boot_config.toml\necho 'cloud-init ran user-data at: ' $(date) >>/home/cpacket/prebootmsg.txt",
    
    * updated the capture_nic_method
    
    * Update main.json
    
    * Update main.json
    
    * updated
    
    * Update main.json
    
    * Updated "cvuv_max_vxlan_ports"
    
    * Update main.json
    
    * removed the "cvuv_max_vxlan_ports"
    
    * Added cvuv_vxlan_dev_0 = "vxlan0" and cvuv_max_vxlan_ports
    
    * Added cvuv_vxlan_dev_X = "vxlanX" all the way to all 10.
    
    * Getting experimental, this will auto add cstors behind cvuv if they are enabled
    
    * Update main.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json
    
    * Update createUiDefinition.json

--- userdata-cvu.bash
+++ userdata-cvu.bash
@@ -1,58 +1,60 @@
 #!/bin/bash
 
-# cVu-V-k inline mode config example
 
-# cvuv_nat_xxx values define NAT passthroughs - up to 4 allowed
-#   (suffix _0,_1,_2, _3)
-#    NOTE : local RESERVED ports 443,80,22,161,162
-# cvuv_nat_loc_ip, cvuv_nat_dst_ip : emptry strings ('') will disable that nat port
+capture_nic_ip=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
 
-# for cvuv_vxlan_srcip, cvuv_vxlan_remoteip : empty strings ('') will disable
-# the vxlan output port.
-
-# cVu-V-k inline
-cat <<EOF_BOOTCFG >/home/cpacket/boot_config.txt
+cat >/home/cpacket/boot_config.toml <<EOF_BOOTCFG
 {
-'vm_type'               : 'azure',
-'capture_mode'          : 'cvuv',
-'cvuv_mode'             : 'inline',
-'cvuv_inline_mode'      : 'tctap',
-'cvuv_mirror_eth_0'     : 'eth0',
-'cvuv_max_vxlan_ports'  : 3,
+"vm_type"               : "azure",
+"cvuv_mode"             : "inline",
+"cvuv_mirror_eth_0"     : "$capture_nic",
 
-'cvuv_vxlan_dev_0'      : 'vxlan0',
-'cvuv_vxlan_srcip_0'    : '',
-'cvuv_vxlan_remoteip_0' : '',
-'cvuv_vxlan_id_0'       : '2110',
-'cvuv_vxlan_eth_0'      : 'cvuv_mirror_eth_0',
+"cvuv_vxlan_srcip_0"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_0" : "",
+"cvuv_vxlan_id_0"       : "1337",
 
-'cvuv_vxlan_dev_1'      : 'vxlan1',
-'cvuv_vxlan_srcip_1'    : '',
-'cvuv_vxlan_remoteip_1' : '',
-'cvuv_vxlan_id_1'       : '2211',
-'cvuv_vxlan_eth_1'      : 'cvuv_mirror_eth_0',
+"cvuv_vxlan_srcip_1"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_1" : "",
+"cvuv_vxlan_id_1"       : "1338",
 
-'cvuv_vxlan_dev_2'      : 'vxlan2',
-'cvuv_vxlan_srcip_2'    : '',
-'cvuv_vxlan_remoteip_2' : '',
-'cvuv_vxlan_id_2'       : '2212',
-'cvuv_vxlan_eth_2'      : 'cvuv_mirror_eth_0',
+"cvuv_vxlan_srcip_2"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_2" : "",
+"cvuv_vxlan_id_2"       : "1339",
 
-'cvuv_nat_loc_proto_0'  : 'tcp',
-'cvuv_nat_loc_ip_0'     : '',
-'cvuv_nat_loc_port_0'   : '',
-'cvuv_nat_dst_ip_0'     : '',
-'cvuv_nat_dst_port_0'   : '',
+"cvuv_vxlan_srcip_3"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_3" : "",
+"cvuv_vxlan_id_3"       : "1340",
 
-'burnside_mode'         : False,
-'cstor_lite_mode'       : False,
-'ssh'                   : {'enabled': True},
-'capture_nic_eth' 	    : 'eth0',
-'management_nic_eth'	: 'eth0',
+"cvuv_vxlan_srcip_4"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_4" : "",
+"cvuv_vxlan_id_4"       : "1341",
+
+"cvuv_vxlan_srcip_5"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_5" : "",
+"cvuv_vxlan_id_5"       : "1342",
+
+"cvuv_vxlan_srcip_6"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_6" : "",
+"cvuv_vxlan_id_6"       : "1343",
+
+"cvuv_vxlan_srcip_7"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_7" : "",
+"cvuv_vxlan_id_7"       : "1344",
+
+"cvuv_vxlan_srcip_8"    : "$capture_nic_ip",
+"cvuv_vxlan_remoteip_8" : "",
+"cvuv_vxlan_id_8"       : "1345",
+
 }
 EOF_BOOTCFG
 
-# make writable so that next boot can overwrite if need be
-chmod ug+w /home/cpacket/boot_config.txt
 
+
+IFS=' ' read -ra ADDR <<< "$REMOTE_IPS"
+for i in "${!ADDR[@]}"; do
+  # Set the remote IPs in the cvu_vxlan_remoteip_* variables
+  sed -i "s|'cvuv_vxlan_remoteip_$i' : '',|'cvuv_vxlan_remoteip_$i' : '${ADDR[i]}',|" /home/cpacket/boot_config.toml
+done
+
+chmod ug+w /home/cpacket/boot_config.toml
 echo "cloud-init ran user-data at: " $(date) >>/home/cpacket/prebootmsg.txt
